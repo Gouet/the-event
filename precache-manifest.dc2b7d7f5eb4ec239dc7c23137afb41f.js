257498ae3c0ccb2dc7b618c4d8efe08ebb206afa
Gouet <victor.gouet@epitech.eu>
2020-03-13 19:21:28 +0100

    Updates

---
+++ precache-manifest.dc2b7d7f5eb4ec239dc7c23137afb41f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a10a6bb27697f244a3cfda9978048c",
+    "url": "/the-event/index.html"
+  },
+  {
+    "revision": "ad0399b9fae6cbb9c95c",
+    "url": "/the-event/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "09a2f10a22a0a4a26a21",
+    "url": "/the-event/static/js/2.6e5680ab.chunk.js"
+  },
+  {
+    "revision": "7f3d623d03408f9350c3b5e194511d01",
+    "url": "/the-event/static/js/2.6e5680ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0399b9fae6cbb9c95c",
+    "url": "/the-event/static/js/main.5c3cf7c5.chunk.js"
+  },
+  {
+    "revision": "ff9cfa771c2dd5b34b78",
+    "url": "/the-event/static/js/runtime-main.168fae74.js"
+  }
+]);
