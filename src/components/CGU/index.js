329a75b99aaa237145046de2cc3643e0e481b48c
Gouet <victor.gouet@epitech.eu>
2020-03-13 20:36:17 +0100

    CGU

--- src/components/CGU/index.js
+++ src/components/CGU/index.js
@@ -3,6 +3,58 @@ import React from 'react';
 const CGU = () => (
   <div>
     <h1>CGU</h1>
+    <div class="bt_bb_port">
+        <div class="bt_bb_cell">
+            <div class="bt_bb_cell_inner">
+                <div class="bt_bb_row" data-structure="12">
+                    <div class="bt_bb_column col-md-12 col-ms-12 bt_bb_align_left bt_bb_vertical_align_top bt_bb_padding_double" data-width="12">
+                        <div class="bt_bb_column_content">
+                            <div class="bt_bb_column_content_inner">
+                                <header class="bt_bb_headline bt_bb_color_scheme_5 bt_bb_dash_none bt_bb_size_normal bt_bb_subheadline bt_bb_align_inherit">
+                                    <h2>
+                                        <span class="bt_bb_headline_content">
+                                        <span>CONDITIONS GENERALES D'UTILISATION</span>
+                                        </span>
+                                        </h2><div class="bt_bb_headline_subheadline">En vigueur au 13/03/2020</div></header><div class="bt_bb_separator bt_bb_top_spacing_medium bt_bb_border_style_none"></div><div class="bt_bb_text"><p>Les présentes conditions générales d’utilisation (dites « CGU ») ont pour objet l’encadrement juridique des modalités de mise à disposition du site et des services par et de définir les conditions d’accès et d’utilisation des services par « l’Utilisateur ».</p>
+<p>Les présentes CGU sont accessibles sur le site à la rubrique «CGU».</p>
+<h3>Article 1 : Les mentions légales</h3>
+<p>L’édition du site https://gouet.github.io/the-event-app est assurée par l'auto-entreprise Victor Gouet au capital de 125 euros, le numéro SIREN est 881 827 612, dont le siège social est situé au 54 rue Marius Lacroix</p>
+<p>Numéro de téléphone +33 6 06 73 10 67</p>
+<p>Adresse e-mail : fenger.company@gmail.com.</p>
+<p>Le Directeur de la publication est : Victor Gouet</p>
+<h3>ARTICLE 2&nbsp;: Accès au site</h3>
+<p>Le site <em>https://gouet.github.io/the-event-app</em> permet à l’Utilisateur un accès gratuit aux services suivants :</p>
+<p>Le site internet propose les services suivants :</p>
+<p>Informations, évènements, soirées, fêtes</p>
+<p>Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l’Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.</p>
+<h3>ARTICLE 3 : Collecte des données</h3>
+<p>Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans la mesure où il ne collecte aucune donnée concernant les Utilisateurs.</p>
+<h3>ARTICLE 4&nbsp;: Propriété intellectuelle</h3>
+<p>Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…) font l’objet d’une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d’auteur.</p>
+<p>La marque The Event est une marque déposée par The Event. Toute représentation et/ou reproduction et/ou exploitation partielle ou totale de cette marque, de quelque nature que ce soit, est totalement prohibée.</p>
+<p>L’Utilisateur doit solliciter l’autorisation préalable du site pour toute reproduction, publication, copie des différents contenus. Il s’engage à une utilisation des contenus du site dans un cadre strictement privé, toute utilisation à des fins commerciales et publicitaires est strictement interdite.</p>
+<p>Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l’autorisation expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’article L 335-2 et suivants du Code de la propriété intellectuelle.</p>
+<p>Il est rappelé conformément à l’article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.</p>
+<h3>ARTICLE 5&nbsp;: Responsabilité</h3>
+<p>Les sources des informations diffusées sur le site https://gouet.github.io/the-event-app sont réputées fiables mais le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions.</p>
+<p>Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle. Malgré des mises à jour régulières, le site <em>https://gouet.github.io/the-event-app&nbsp;</em>ne peut être tenu responsable de la modification des dispositions administratives et juridiques survenant après la publication. De même, le site ne peut être tenue responsable de l’utilisation et de l’interprétation de l’information contenue dans ce site.</p>
+<p>Le site <em>https://gouet.github.io/the-event-app&nbsp;</em>ne peut être tenu pour responsable d’éventuels virus qui pourraient infecter l’ordinateur ou tout matériel informatique de l’Internaute, suite à une utilisation, à l’accès, ou au téléchargement provenant de ce site.</p>
+<p>La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d’un tiers.</p>
+<h3>ARTICLE 6&nbsp;: Liens hypertextes</h3>
+<p>Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces liens, il sortira du site <em>https://gouet.github.io/the-event-app.</em> Ce dernier n’a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.</p>
+<h3>ARTICLE 7 : Cookies</h3>
+<p>L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation.</p>
+<p>Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de l’Utilisateur par votre navigateur et qui sont nécessaires à l’utilisation du site <em>https://gouet.github.io/the-event-app</em>. Les cookies ne contiennent pas d’information personnelle et ne peuvent pas être utilisés pour identifier quelqu’un. Un cookie contient un identifiant unique, généré aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l’Utilisateur, d’autres restent.</p>
+<p>L’information contenue dans les cookies est utilisée pour améliorer le site <em>https://gouet.github.io/the-event-app</em>.</p>
+<p>En naviguant sur le site, L’Utilisateur les accepte.</p>
+<p>L’Utilisateur doit toutefois donner son consentement quant à l’utilisation de certains cookies.</p>
+<p>A défaut d’acceptation, l’Utilisateur est informé que certaines fonctionnalités ou pages risquent de lui être refusées.</p>
+<p>L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.</p>
+<h3>ARTICLE 8&nbsp;: Droit applicable et juridiction compétente</h3>
+<p>La législation française s’applique au présent contrat. En cas d’absence de résolution amiable d’un litige né entre les parties, les tribunaux français seront seuls compétents pour en connaître.</p>
+<p>Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux coordonnées inscrites à l’ARTICLE 1.</p>
+<p>Réalisé sur https://www.legalplace.fr</p>
+</div><div class="bt_bb_separator bt_bb_top_spacing_medium bt_bb_border_style_none"></div></div></div></div></div></div></div></div>
   </div>
 );
 
